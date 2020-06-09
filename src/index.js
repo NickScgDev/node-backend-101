37fc2c6a66911454c944a27968d84b40ba05eb97
Chaiwtun <chaiwtun@scg.com>
2020-06-09 22:40:12 +0700

    [init-app-with-node] create http server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('hello world');
+const http = require("http");
+
+const port = 3000;
+
+const server = http.createServer(function (request, response) {
+  response.writeHead(200, {
+    "Content-Type": "text/html;charset=utf-8;"
+  });
+
+  response.end("Hello world");
+});
+
+server.listen(port, function () {
+  console.log('listen on port:', port);
+});
